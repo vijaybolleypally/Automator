a0c542eb6ba7366d33512b096626c73a247d46c9
Vijay Bolleypally <vijay.bolleypally@autodesk.com>
2016-01-16 17:08:14 +0530

    added chrome driver

--- src/test/code/selenium/config/Configuration.java
+++ src/test/code/selenium/config/Configuration.java
@@ -1,6 +1,7 @@
 package selenium.config;
 
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import selenium.lib.AccessPropertiesFile;
 
@@ -8,9 +9,12 @@ import selenium.lib.AccessPropertiesFile;
  * Created by t_bollv on 12/12/15.
  */
 public class Configuration {
-    String configFilePath = System.getProperty("user.dir")+"/src/test/resources/resources1/configMe.properties".replace("/",System.getProperty("file.separator"));
-    public AccessPropertiesFile configFileObj;
-    WebDriver driver;
+
+    private String fileSeparatot = System.getProperty("file.separator");
+    private String projectNameDirectory = System.getProperty("user.dir");
+    private String configFilePath = projectNameDirectory + "/src/test/resources/SeleniumConfig.properties".replace("/", fileSeparatot);
+    private AccessPropertiesFile configFileObj;
+    public WebDriver driver;
 
     public Configuration() throws Exception {
         configFileObj = new AccessPropertiesFile(configFilePath);
@@ -18,10 +22,21 @@ public class Configuration {
 
     public WebDriver createDriverFromConfig() {
         String requiredDriverValue = configFileObj.getPropValues("browser");
+
         if (requiredDriverValue.equalsIgnoreCase("firefox")) {
             driver = new FirefoxDriver();
+        } else if (requiredDriverValue.equalsIgnoreCase("chrome")) {
+            String pathToChromedriver = projectNameDirectory;
+            if (System.getProperty("os.name").toLowerCase().contains("windows")) {
+                pathToChromedriver =  pathToChromedriver + "/src/test/resources/lib/windows/chromedriver.exe".replace("/", fileSeparatot);
+            } else if (System.getProperty("os.name").toLowerCase().contains("mac")) {
+                pathToChromedriver = pathToChromedriver + "/src/test/resources/lib/mac/chromedriver".replace("/", fileSeparatot);
+            }
+            System.setProperty("webdriver.chrome.driver", pathToChromedriver);
+            driver = new ChromeDriver();
+
         } else {
-            System.out.print("NO Such Drive Defined");
+            System.out.print("check browser value in SeleniumConfig.properties");
         }
         return driver;
     }
